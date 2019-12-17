c844fd07a5864d6c8fb7968e66b5444fd624e862
Matt45D <Matt45Davison@gmail.com>
2019-12-17 14:45:45 -0800

    Added in XML comments to MicInput and removed the unutilized asset SuperCyan

---
+++ Assets/Scripts/DictationScript.cs
@@ -0,0 +1,73 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
+
+public class DictationScript : MonoBehaviour
+{
+    [SerializeField]
+    private Button SentButton;
+    //private Text m_Hypotheses;
+
+    [SerializeField]
+    private InputField m_Recognitions;
+
+    [SerializeField]
+    private Text m_condidence;
+
+    [SerializeField]
+    private MicInput mic;
+
+    private DictationRecognizer m_DictationRecognizer;
+
+    void Start()
+    {
+        m_DictationRecognizer = new DictationRecognizer();
+        // m_DictationRecognizer.AutoSilenceTimeoutSeconds = 50;
+
+        Debug.Log("Dictation Recognizer is being initialized");
+
+        m_DictationRecognizer.DictationResult += (text, confidence) =>
+        {
+            Debug.LogFormat("Dictation result: {0}", text);
+            m_Recognitions.text = text;
+            SentButton.GetComponent<DialogFlow>().SendText(text);
+            m_condidence.text = confidence.ToString();
+            //;
+        };
+
+        m_DictationRecognizer.DictationHypothesis += (text) =>
+        {
+            Debug.LogFormat("Dictation hypothesis: {0}", text);
+        };
+
+        m_DictationRecognizer.DictationError += (error, hresult) =>
+        {
+            Debug.LogErrorFormat("Dictation error: {0}; HResult = {1}.", error, hresult);
+        };
+        m_DictationRecognizer.DictationComplete += SessionEnd;
+        m_DictationRecognizer.Start();
+    }
+
+    public void startDitection() {
+        if (m_DictationRecognizer.Status != SpeechSystemStatus.Running) {
+            m_DictationRecognizer.Start();
+        }
+        else
+        {
+            Debug.Log("SpeechSystemStatus is Running");
+        }
+    }
+
+    public void stopDitection()
+    {
+        if (m_DictationRecognizer.Status == SpeechSystemStatus.Running)
+        {
+            m_DictationRecognizer.Stop();
+        }
+    }
+
+    public void SessionEnd(DictationCompletionCause cause)
+    {
+    }
+}
--- Assets/Scripts/MicInput.cs
+++ Assets/Scripts/MicInput.cs
@@ -8,28 +8,40 @@ public class MicInput : MonoBehaviour
     public float MicLoudness;
     public float thre;
     public DictationScript dictationScript;
+    public int _sampleWindow = 128;
+    public AudioClip _clipRecord;// = AudioClip.Create("MySinusoid", 44100 * 2, 1, 44100, true);
     private string _device;
+    private float time = 0;
+    private bool _isInitialized;
 
+    /// <summary>
+    /// Unity initialization function that will set MicLoudness to zero
+    /// </summary>
     void start() {
         MicLoudness = 0f;
     }
-    //mic initialization
+    
+    /// <summary>
+    /// Member Function for microphone initialization
+    /// </summary>
     void InitMic()
     {
+        // If no microphone device is connected to the computer or the project settings havent been setup correctly _device will be set to zero and output message will be displayed in the console
         if (_device == null) _device = Microphone.devices[0];
         _clipRecord = Microphone.Start(_device, true, 999, 44100);
     }
 
+    /// <summary>
+    /// Member Function to stop microphone recording
+    /// </summary>
     void StopMicrophone()
     {
         Microphone.End(_device);
     }
 
-    int _sampleWindow = 128;
-    AudioClip _clipRecord;// = AudioClip.Create("MySinusoid", 44100 * 2, 1, 44100, true);
-    
-
-    //get data from microphone into audioclip
+    /// <summary>
+    /// Gets data from the microphone and stores it into an audioclip. The wavepeak of the audioclip is compared with levelMax and returns levelMax
+    /// </summary>
     float LevelMax()
     {
         float levelMax = 0;
@@ -41,6 +53,7 @@ public class MicInput : MonoBehaviour
         for (int i = 0; i < _sampleWindow; i++)
         {
             float wavePeak = waveData[i] * waveData[i];
+            // Compares levelMax with wavePeak, if wavePeak is larger than that becomes the new levelMax
             if (levelMax < wavePeak)
             {
                 levelMax = wavePeak;
@@ -49,8 +62,9 @@ public class MicInput : MonoBehaviour
         return levelMax;
     }
 
-
-    private float time = 0;
+    /// <summary>
+    /// Unity Update function, If the levelMax is greater than a set value of threshold then it will call dictationScript.startDitection, Else it will call dictationScript.stopDitection in 2 seconds
+    /// </summary>
     void Update()
     {
         // levelMax equals to the highest normalized value power 2, a small number because < 1
@@ -70,27 +84,35 @@ public class MicInput : MonoBehaviour
         }
     }
 
-    bool _isInitialized;
-    // start mic when scene starts
+    /// <summary>
+    /// Starts the microphone by calling InitMic function when the scene starts up
+    /// </summary>
     void OnEnable()
     {
         InitMic();
         _isInitialized = true;
     }
 
-    //stop mic when loading a new level or quit application
+    /// <summary>
+    /// Stops the microphone by calling StopMicrophone function when loading a new scene
+    /// </summary>
     void OnDisable()
     {
         StopMicrophone();
     }
 
+    /// <summary>
+    /// Deconstructor that stops the microphone by calling StopMicrophone function when closing the application
+    /// </summary>
     void OnDestroy()
     {
         StopMicrophone();
     }
 
-
-    // make sure the mic gets started & stopped when application gets focused
+    /// <summary>
+    /// Starts the microphone recording by calling InitMic function whenever the application is focused and _isInitialized is set to False
+    /// </summary>
+    /// <param name="focus"></param>
     void OnApplicationFocus(bool focus)
     {
         if (focus)
