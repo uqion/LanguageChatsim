698d77f778f3bffc1405d613815c8ec9bfa8f795
uqion <juliachu312@gmail.com>
2020-02-19 07:01:52 -0800

    transaction node

---
+++ Assets/Scripts/SO_Database.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SO_Database : ScriptableObject
+{
+    /// <summary>
+    /// grocery items and their price that are available to be purchased .
+    /// </summary>
+    private Dictionary<string, double> itemPriceDatabase = new Dictionary<string, double>()
+    {
+        {"Bottle of Water", 2.99}
+    };
+
+    public double GetPrice(string item)
+    {
+        return itemPriceDatabase[item];
+    }
+}
